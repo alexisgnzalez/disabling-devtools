249b52ea77a2f69854a30de28e887ed9ba19c2e7
Alexis Gonzalez <1991.alexis@gmail.com>
2024-01-19 15:48:58 -0400

    moved logic to service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,48 +7,9 @@ import { DevtoolsService } from './devtools.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'disabling-devtools';
-  time: number = 0;
   message: string = '';
-  tableTime: number = 0;
-  logTime: number = 0;
 
-  /* Elemento que vamos a imprimir en consola */
-  elemento_relleno: Array<Array<number>> = [];
-
-  constructor(private devtools: DevtoolsService) {
+  constructor(public devtools: DevtoolsService) {
     devtools.init();
-    let aux: Array<number> = [];
-    for (let i = 0; i < 20; i++) {
-      aux.push(i * 997);
-    }
-    for (let i = 0; i < 50; i++) {
-      this.elemento_relleno.push([...aux]);
-    }
-
-    setInterval(() => {
-      this.tableTime = this.measureTime(() =>
-        console.table(this.elemento_relleno)
-      );
-      this.logTime = this.measureTime(() => console.log(this.elemento_relleno));
-      console.clear();
-
-      if (this.tableTime > this.nonZeroLog(this.logTime) * 10) {
-        this.message = 'Dev Tools abierto';
-        window.location.href = 'about:blank';
-      } else {
-        this.message = 'Dev Tools cerrado';
-      }
-    }, 1000);
-  }
-
-  measureTime(logData: () => void) {
-    const initTime = new Date().getTime();
-    logData();
-    return new Date().getTime() - initTime;
-  }
-
-  nonZeroLog(logTime: number) {
-    return logTime <= 0 ? 1 : logTime;
   }
 }
--- src/app/devtools.service.ts
+++ src/app/devtools.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DevtoolsService {
+  tableTime: number = 0;
+  logTime: number = 0;
+  placeholderLog: Array<Array<number>> = [];
 
-  constructor() { }
+  constructor() {
+    this.fillPlaceholder();
+  }
+
+  fillPlaceholder() {
+    let aux: Array<number> = [];
+    for (let i = 0; i < 20; i++) {
+      aux.push(i * 997);
+    }
+    for (let i = 0; i < 50; i++) {
+      this.placeholderLog.push([...aux]);
+    }
+  }
+
+  measureTime(logData: () => void) {
+    const initTime = new Date().getTime();
+    logData();
+    return new Date().getTime() - initTime;
+  }
+
+  nonZeroLog(logTime: number) {
+    return logTime <= 0 ? 1 : logTime;
+  }
+
+  init() {
+    setInterval(() => {
+      this.tableTime = this.measureTime(() =>
+        console.table(this.placeholderLog)
+      );
+      this.logTime = this.measureTime(() => console.log(this.placeholderLog));
+      console.clear();
+
+      if (this.tableTime > this.nonZeroLog(this.logTime) * 10) {
+        window.location.href = 'about:blank';
+      }
+    }, 1000);
+  }
 }
